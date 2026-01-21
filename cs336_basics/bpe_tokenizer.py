5070a58ea9e95ca28eeec3aff9b8df32f02101a2
Uğur Ali Kaplan <uguralikaplan@gmail.com>
2026-01-21 23:14:48 +0100

    Optimized run performance

--- cs336_basics/bpe_tokenizer.py
+++ cs336_basics/bpe_tokenizer.py
@@ -3,12 +3,12 @@ from collections.abc import Iterable, Iterator
 import pickle
 from pathlib import Path
 import regex as re
-from copy import deepcopy
+import numpy as np
 
-import os
-import json
-from functools import lru_cache
-import tiktoken
+try:
+    from .utils import sample_from_txt, benchmark_tokenizer
+except ImportError:
+    from utils import sample_from_txt, benchmark_tokenizer
 
 
 class Tokenizer:
@@ -62,15 +62,27 @@ class Tokenizer:
                     catch = (match.group(0)).encode("utf-8")
                     chunk_tokens = [self.byte_to_id[bytes([c])] for c in catch]
                     for merge_pair in self.merges:
-                        chunk_tokens = self.merge_pairs(chunk_tokens, merge_pair)
+                        if (self.byte_to_id[merge_pair[0]] in chunk_tokens) and (
+                            self.byte_to_id[merge_pair[1]] in chunk_tokens
+                        ):
+                            chunk_tokens = self.merge_pairs(chunk_tokens, merge_pair)
+                        else:
+                            continue
 
                     encoded_sequence.extend(chunk_tokens)
 
         return encoded_sequence
 
-    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
-        for it in iterable:
-            yield from self.encode(it)
+    def encode_iterable(self, iterable: Iterable[str], chunk_size: int = 1024 * 1024) -> Iterator[int]:
+        buffer = ""
+        for item in iterable:
+            buffer += item
+            if len(buffer) >= chunk_size:
+                yield from self.encode(buffer)
+                buffer = ""
+
+        if buffer:
+            yield from self.encode(buffer)
 
     def decode(self, ids: list[int]) -> str:
         comb = []
@@ -84,121 +96,89 @@ class Tokenizer:
     def merge_pairs(self, sequence: list[int], old_tokens: tuple[bytes]) -> list[int]:
         old_token_ids = (self.byte_to_id[old_tokens[0]], self.byte_to_id[old_tokens[1]])
         new_token_id = self.byte_to_id[old_tokens[0] + old_tokens[1]]
-        new_sequence = deepcopy(sequence)
-        num_inserts = 0
+        new_sequence = []
         i = 0
-        while i < (len(sequence) - 1):
-            if (sequence[i] == old_token_ids[0]) and (sequence[i + 1] == old_token_ids[1]):
-                new_sequence[(i - num_inserts) : (i - num_inserts + 2)] = [new_token_id]
-                num_inserts += 1
+        while i < len(sequence):
+            if (i < len(sequence) - 1) and (sequence[i] == old_token_ids[0]) and (sequence[i + 1] == old_token_ids[1]):
+                new_sequence.append(new_token_id)
+                i += 2
+            else:
+                new_sequence.append(sequence[i])
                 i += 1
-            i += 1
         return new_sequence
 
 
 def main():
-    @lru_cache
-    def gpt2_bytes_to_unicode() -> dict[int, str]:
-        """
-        Returns a mapping between every possible byte (an integer from 0 to 255) to a
-        printable unicode string character representation. This function is taken
-        from the GPT-2 code.
-
-        For example, `chr(0)` is `\x00`, which is an unprintable character:
-
-        >>> chr(0)
-        '\x00'
-        >>> print(chr(0))
-
-        As a result, this function returns a dictionary `d` where `d[0]` returns `Ā`.
-        The bytes that are visually printable keep their original string representation [1].
-        For example, `chr(33)` returns `!`, and so accordingly `d[33]` returns `!`.
-        Note in particular that the space character `chr(32)` becomes `d[32]`, which
-        returns 'Ġ'.
-
-        For unprintable characters, the function shifts takes the integer representing
-        the Unicode code point of that character (returned by the Python `ord`) function
-        and shifts it by 256. For example, `ord(" ")` returns `32`, so the the space character
-        ' ' is shifted to `256 + 32`. Since `chr(256 + 32)` returns `Ġ`, we use that as the
-        string representation of the space.
-
-        This function can simplify the BPE implementation and makes it slightly easier to
-        manually inspect the generated merges after they're serialized to a file.
-        """
-        # These 188 integers can used as-is, since they are not whitespace or control characters.
-        # See https://www.ssec.wisc.edu/~tomw/java/unicode.html.
-        bs = (
-            list(range(ord("!"), ord("~") + 1))
-            + list(range(ord("¡"), ord("¬") + 1))
-            + list(range(ord("®"), ord("ÿ") + 1))
-        )
-        cs = bs[:]
-        # now get the representations of the other 68 integers that do need shifting
-        # each will get mapped chr(256 + n), where n will grow from 0...67 in the loop
-        # Get printable representations of the remaining integers 68 integers.
-        n = 0
-        for b in range(2**8):
-            if b not in bs:
-                # If this integer isn't in our list of visually-representable
-                # charcters, then map it to the next nice character (offset by 256)
-                bs.append(b)
-                cs.append(2**8 + n)
-                n += 1
-        characters = [chr(n) for n in cs]
-        d = dict(zip(bs, characters))
-        return d
-
-    def get_tokenizer_from_vocab_merges_path(
-        vocab_path: str | os.PathLike,
-        merges_path: str | os.PathLike,
-        special_tokens: list[str] | None = None,
-    ):
-        gpt2_byte_decoder = {v: k for k, v in gpt2_bytes_to_unicode().items()}
-        with open(vocab_path) as vocab_f:
-            gpt2_vocab = json.load(vocab_f)
-        gpt2_bpe_merges = []
-        with open(merges_path) as f:
-            for line in f:
-                cleaned_line = line.rstrip()
-                if cleaned_line and len(cleaned_line.split(" ")) == 2:
-                    gpt2_bpe_merges.append(tuple(cleaned_line.split(" ")))
-        # The GPT-2 tokenizer uses a remapped unicode encoding for bytes. Let's
-        # just return the original bytes, so we don't force students to use
-        # any particular encoding scheme.
-        vocab = {
-            gpt2_vocab_index: bytes([gpt2_byte_decoder[token] for token in gpt2_vocab_item])
-            for gpt2_vocab_item, gpt2_vocab_index in gpt2_vocab.items()
-        }
-        # If any of the special tokens don't exist in the vocab, append them to the vocab.
-        if special_tokens:
-            for special_token in special_tokens:
-                byte_encoded_special_token = special_token.encode("utf-8")
-                if byte_encoded_special_token not in set(vocab.values()):
-                    vocab[len(vocab)] = byte_encoded_special_token
-
-        merges = [
-            (
-                bytes([gpt2_byte_decoder[token] for token in merge_token_1]),
-                bytes([gpt2_byte_decoder[token] for token in merge_token_2]),
-            )
-            for merge_token_1, merge_token_2 in gpt2_bpe_merges
-        ]
-        return Tokenizer(vocab, merges, special_tokens)
-
-    reference_tokenizer = tiktoken.get_encoding("gpt2")
-    tokenizer = get_tokenizer_from_vocab_merges_path(
-        vocab_path="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/tests/fixtures/gpt2_vocab.json",
-        merges_path="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/tests/fixtures/gpt2_merges.txt",
+    owt_tokenizer = Tokenizer.from_files(
+        vocab_filepath="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/owt_vocab.pickle",
+        merges_filepath="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/owt_merges.pickle",
+        special_tokens=["<|endoftext|>"],
     )
-    corpus_path = "/home/ugurkap/stanford-cs336-assignments/assignment1-basics/tests/fixtures/address.txt"
-    with open(corpus_path) as f:
-        corpus_contents = f.read()
-    reference_ids = reference_tokenizer.encode(corpus_contents)
-    ids = tokenizer.encode(corpus_contents)
-    assert ids == reference_ids
+    tiny_tokenizer = Tokenizer.from_files(
+        vocab_filepath="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/tiny_vocab.pickle",
+        merges_filepath="/home/ugurkap/stanford-cs336-assignments/assignment1-basics/tiny_merges.pickle",
+        special_tokens=["<|endoftext|>"],
+    )
+    # Benchmarking
 
-    assert tokenizer.decode(ids) == corpus_contents
-    assert reference_tokenizer.decode(reference_ids) == corpus_contents
+    owt_samples = sample_from_txt(
+        "/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/owt_valid.txt", num_samples=10
+    )
+    tiny_samples = sample_from_txt(
+        "/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/TinyStoriesV2-GPT4-valid.txt", num_samples=10
+    )
+
+    benchmark_tokenizer(owt_tokenizer, owt_samples, "<|endoftext|>", "OWT")
+    print()
+    benchmark_tokenizer(tiny_tokenizer, tiny_samples, "<|endoftext|>", "TinyStories")
+    print()
+    benchmark_tokenizer(owt_tokenizer, tiny_samples, "<|endoftext|>", "OWT Tokenizer on TinyStories Dataset")
+    print()
+    benchmark_tokenizer(tiny_tokenizer, owt_samples, "<|endoftext|>", "TinyStories Tokenizer on OWT Dataset")
+
+    # Process the training and validation sets
+    owt_train_out = []
+    with open(Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/owt_train.txt").absolute()) as f:
+        for _id in owt_tokenizer.encode_iterable(f):
+            owt_train_out.append(_id)
+    np.save(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/owt_train_tokens.npy").absolute(),
+        np.array(owt_train_out, dtype=np.uint16),
+    )
+    del owt_train_out
+
+    owt_valid_out = []
+    with open(Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/owt_valid.txt").absolute()) as f:
+        for _id in owt_tokenizer.encode_iterable(f):
+            owt_valid_out.append(_id)
+    np.save(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/owt_valid_tokens.npy").absolute(),
+        np.array(owt_valid_out, dtype=np.uint16),
+    )
+    del owt_valid_out
+
+    tiny_train_out = []
+    with open(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/TinyStoriesV2-GPT4-train.txt").absolute()
+    ) as f:
+        for _id in tiny_tokenizer.encode_iterable(f):
+            tiny_train_out.append(_id)
+    np.save(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/tiny_train_tokens.npy").absolute(),
+        np.array(tiny_train_out, dtype=np.uint16),
+    )
+    del tiny_train_out
+
+    tiny_valid_out = []
+    with open(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/TinyStoriesV2-GPT4-valid.txt").absolute()
+    ) as f:
+        for _id in tiny_tokenizer.encode_iterable(f):
+            tiny_valid_out.append(_id)
+    np.save(
+        Path("/home/ugurkap/stanford-cs336-assignments/assignment1-basics/data/tiny_valid_tokens.npy").absolute(),
+        np.array(tiny_valid_out, dtype=np.uint16),
+    )
 
 
 if __name__ == "__main__":
